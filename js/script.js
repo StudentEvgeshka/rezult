39edcbbc6eafa21cc9a2e7a9f9933479e8d730b8
StudentEvgeshka <43698839+StudentEvgeshka@users.noreply.github.com>
2018-11-04 08:54:43 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,76 +1,82 @@
-/*первая каруселька*/
 $(document).ready(function() {
-  // Owl Carousel
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    	loop: true,
-    	nav: true,
-    	items:             4,
-		// 4 блоков на компьютерах (экран от 1200px до 901px)
-		itemsDesktop:      [900, 3],
-		// 2 блоков на компьютерах (экран от 1400px до 901px)
-		itemsDesktopSmall: [700, 2],
-		// 1 элемента на планшетах (экран от 700 до 480 пикселей)
-		itemsTablet:       [500, 1],
-		// Настройки для телефона отключены, в этом случае будут
-	// использованы настройки планшета
-		itemsMobile:       false
-  					});
- // Назад
-// При клике на "Назад"
-$("#js-prev").click(function () {
+    //
+    // Auto heihgt========================
+    // 
+    var videoBlockHeight = $('.video-block').height(); 
+    $('.news-block').css('height', videoBlockHeight+'px');
+    // on resize
+    $( window ).resize(function() {
+        var videoBlockHeight = $('.video-block').height(); 
+        $('.news-block').css('height', videoBlockHeight+'px');
+    });
 
-	// Запускаем перемотку влево
-	owl.trigger('owl.prev');
 
-	return false;
-});
+    //
+    // Owl Carousel 1 ========================
+    // 
+    var owl1 = $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        items: 4,
+        // 4 блоков на компьютерах (экран от 1200px до 901px)
+        itemsDesktop: [900, 3],
+        // 2 блоков на компьютерах (экран от 1400px до 901px)
+        itemsDesktopSmall: [700, 2],
+        // 1 элемента на планшетах (экран от 700 до 480 пикселей)
+        itemsTablet: [500, 1],
+        // Настройки для телефона отключены, в этом случае будут
+        // использованы настройки планшета
+        itemsMobile: false
+    });
 
-// Вперед
-// При клике на "Вперед"
-$('#js-next').click(function () {
+    // Назад
+    // При клике на "Назад"
+    $("#js-prev").click(function() {
+        // Запускаем перемотку влево
+        owl1.trigger('owl.prev');
+        return false;
+    });
 
-	// Запускаем перемотку вправо
-	owl.trigger('owl.next');
+    // Вперед
+    // При клике на "Вперед"
+    $('#js-next').click(function() {
+        // Запускаем перемотку вправо
+        owl1.trigger('owl.next');
+        return false;
+    });
 
-	return false;
-});
-});
-/*вторая*/
-$(document).ready(function() {
-  // Owl Carousel
-  var owl = $(".owl-carousel2");
-  owl.owlCarousel({
-    	loop: true,
-    	nav: true,
-    	items:             5,
-		// 4 блоков на компьютерах (экран от 1200px до 901px)
-		itemsDesktop:      [900, 4],
-		// 2 блоков на компьютерах (экран от 1400px до 901px)
-		itemsDesktopSmall: [700, 3],
-		// 1 элемента на планшетах (экран от 700 до 480 пикселей)
-		itemsTablet:       [500, 1],
-		// Настройки для телефона отключены, в этом случае будут
-	// использованы настройки планшета
-		itemsMobile:       false
-  					});
- // Назад
-// При клике на "Назад"
-$("#k-prev").click(function () {
 
-	// Запускаем перемотку влево
-	owl.trigger('owl.prev');
+    //
+    // Owl Carousel 2 ========================
+    // 
+    var owl2 = $(".owl-carousel2").owlCarousel({
+        loop: true,
+        nav: true,
+        items: 5,
+        // 4 блоков на компьютерах (экран от 1200px до 901px)
+        itemsDesktop: [900, 4],
+        // 2 блоков на компьютерах (экран от 1400px до 901px)
+        itemsDesktopSmall: [700, 3],
+        // 1 элемента на планшетах (экран от 700 до 480 пикселей)
+        itemsTablet: [500, 2],
+        // Настройки для телефона отключены, в этом случае будут
+        // использованы настройки планшета
+        itemsMobile: false
+    });
 
-	return false;
-});
+    // Назад
+    // При клике на "Назад"
+    $("#k-prev").click(function() {
+        // Запускаем перемотку влево
+        owl2.trigger('owl.prev');
+        return false;
+    });
 
-// Вперед
-// При клике на "Вперед"
-$('#k-next').click(function () {
-
-	// Запускаем перемотку вправо
-	owl.trigger('owl.next');
-
-	return false;
-});
+    // Вперед
+    // При клике на "Вперед"
+    $('#k-next').click(function() {
+        // Запускаем перемотку вправо
+        owl2.trigger('owl.next');
+        return false;
+    });
 });
